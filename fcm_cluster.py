5bde7a4ee244b339b1edac985264101eaa6f25e4
hamidreza <hrhhemati@gmail.com>
2021-06-04 19:11:57 +0430

    get_data method return numpy 2d array with float elements

--- fcm_cluster.py
+++ fcm_cluster.py
@@ -1,22 +1,26 @@
 import csv
 dataFile = 'data1.csv'
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def extract_input():
+def get_data():
+    points = []
     with open(dataFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         col_num = len(next(csv_reader))  # Read first line and count columns
         csv_file.seek(0)  # go back to beginning of file
-        line_count = 0
-        print(col_num)
         for row in csv_reader:
-            print(f'{", ".join(row)}')
-            line_count += 1
-        print(line_count)
+            points.append(row)
+            # print(f'{", ".join(row)}')
+    print(col_num)
+    points = np.array(points)  # convert 2D list to a 2D numpy array
+    points = points.astype(np.float64) # cast all the elements to float from string
+    return points, col_num
 
 
 def main():
-    extract_input()
+    points, col_num = get_data()
 
 
 main()
