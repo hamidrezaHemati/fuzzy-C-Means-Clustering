5eda1757d7d0f1bf17fcabef9df734837c76239b
hamidreza <hrhhemati@gmail.com>
2021-06-04 11:15:29 +0430

    csv reading method added

--- fcm_cluster.py
+++ fcm_cluster.py
@@ -1,1 +1,24 @@
-print("Hello world!")
+import csv
+dataFile = 'data1.csv'
+
+
+def extract_input():
+    with open(dataFile) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        col_num = len(next(csv_reader))  # Read first line and count columns
+        csv_file.seek(0)  # go back to beginning of file
+        line_count = 0
+        print(col_num)
+        for row in csv_reader:
+            print(f'{", ".join(row)}')
+            line_count += 1
+        print(line_count)
+
+
+def main():
+    extract_input()
+
+
+main()
+
+
